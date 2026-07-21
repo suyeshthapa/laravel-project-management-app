5ec2ec3341bfd6903f3465ff2474cd0b652e3f7a
SuyeshThapa <86250888+SuyeshThapa@users.noreply.github.com>
2026-07-21 20:13:24 +0545

    uploading the project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Zura',
+            'email' => 'zura@example.com',
+            'password' => bcrypt('123.321A'),
+            'email_verified_at' => time()
+        ]);
+
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
+    }
+}
